272829dfb059efca3798dd6d73606cbaab699274
Bananoob <mathis.jardon@gmail.com>
2023-06-29 14:02:00 +0200

    Changed a variable name

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let addNewTask = document.querySelector('#addNewTask'),
-    newTask = document.querySelector('#newTask'),
+    taskField = document.querySelector('#taskField'),
     tasks = document.querySelector('#tasks')
 
 // Display the input field
 addNewTask.addEventListener("click", () => {
     addNewTask.style.display = "none"
-    newTask.style.display = "flex";
-    newTask.focus()
+    taskField.style.display = "flex";
+    taskField.focus()
 })
 
 // Add the task
 let taskId = 1;
-if (newTask.addEventListener("keyup", (e) => {
-    if (e.key === "Enter" && newTask.value !== "") {
+if (taskField.addEventListener("keyup", (e) => {
+    if (e.key === "Enter" && taskField.value !== "") {
         // Variables
         let container = document.createElement("section")
         let task = document.createElement("div");
@@ -21,7 +21,7 @@ if (newTask.addEventListener("keyup", (e) => {
         // Proprities
         task.id = `div-${taskId++}`;
         task.draggable = true;
-        taskP.innerHTML = newTask.value;
+        taskP.innerHTML = taskField.value;
 
         // Append
         tasks.append(container)
@@ -32,12 +32,12 @@ if (newTask.addEventListener("keyup", (e) => {
         drag(task)
         removeTask(task)
 
-        newTask.style.display = "none"
-        newTask.value = ""
+        taskField.style.display = "none"
+        taskField.value = ""
         addNewTask.style.display = "flex"
     } else if (e.key === "Escape") {
-        newTask.style.display = "none"
-        newTask.value = ""
+        taskField.style.display = "none"
+        taskField.value = ""
         addNewTask.style.display = "flex"
     }
 }));
